0d06d174db33c21bebf0030b31a9415a3847b78e
EC2 Default User <ec2-user@ip-172-31-3-195.ap-northeast-1.compute.internal>
2022-12-17 06:24:47 +0000

    start sns

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,14 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.all
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+
+    @books = Book.includes(:favorited_users).
+    sort_by { |x|
+      x.favorited_users.includes(:vavorites).where(created_at: from...to).size
+    }.reverse
+    
     @book = Book.new
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,7 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/search', to: 'searches#search'
+  
+  get 'message/:id', to: 'messages#show', as: 'message'
+  resources :messages, only: [:create]
 end
